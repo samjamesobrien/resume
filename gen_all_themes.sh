7aa6828c8fc6e0761b96c746725107df9c9a7f5a
Sam O'Brien <sobrien@vsware.ie>
2017-07-11 13:16:34 +0100

    use positive theme for readme.md

--- gen_all_themes.sh
+++ gen_all_themes.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 bash generate.sh modern
-#bash generate.sh positive
+bash generate.sh positive
 bash generate.sh compact
 #bash generate.sh minimalist
 #bash generate.sh hello-world
 cp out/resume_positive.md ./README.md
+rm out/resume_positive.*
