a1d124cda75fe3ce9596384dbf458eaf5a5e2b20
Sam O'Brien <sobrien@vsware.ie>
2017-07-13 17:40:58 +0100

    fix ordering in geenrate script

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-rm ./out/resume_${name}.*
 name=${1:-positive}
+rm ./out/resume_${name}.*
 echo "generating ${name} themed resumes"
 hackmyresume BUILD resume.json TO ./out/resume_${name}.html ./out/resume_${name}.md ./out/resume_${name}.doc ./out/resume_${name}.json -t ${name} && \
 wkhtmltopdf -s A4 --dpi 240 ./out/resume_${name}.html ./out/resume_${name}.pdf
